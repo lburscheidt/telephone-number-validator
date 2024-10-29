d061ba62bed52708907eab1749b430e58c98b04a
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-29 23:17:44 +0100

    feat: minor changes to fit freeCodeCamp template

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const clearBtn = document.querySelector("#clear-btn");
 const result = document.querySelector("#results-div");
 
 function isValidNumber(str) {
-  var regex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?(\d{3}[\s\-]?\d{4}$)/;
+  var regex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s-]?(\d{3}[\s-]?\d{4}$)/;
   return regex.test(str);
 }
 
@@ -14,13 +14,18 @@ checkBtn.addEventListener("click", () => {
   if (input.value == "") {
     alert("Please provide a phone number");
   } else if (isValidNumber(input.value)) {
-    const inputValue = input.value;
-    result.innerText = `Valid US number: ${inputValue}`;
+    result.innerHTML = `Valid US number:<br>${input.value}`;
+    input.value = "";
   } else {
-    result.innerText = `Invalid US number: ${inputValue}`;
+    result.innerHTML = `Invalid US number:<br>${input.value}`;
+    input.value = "";
   }
 });
 
 clearBtn.addEventListener("click", () => {
+  result.innerText = "";
+});
+
+input.addEventListener("focus", () => {
   result.innerText = "";
 });
