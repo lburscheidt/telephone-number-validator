46d7f80496c8408424bba04b83d5c5adb6e59e1b
lburscheidt <176681558+lburscheidt@users.noreply.github.com>
2024-10-29 22:16:22 +0100

    feat: add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./style.css";
-console.log("hello");
-console.log("world");
+
+const input = document.querySelector("#user-input");
+const checkBtn = document.querySelector("#check-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const result = document.querySelector("#results-div");
+
+function isValidNumber(str) {
+  var regex = /^(1\s?)?(\d{3}|\(\d{3}\))[\s\-]?(\d{3}[\s\-]?\d{4}$)/;
+  return regex.test(str);
+}
+
+checkBtn.addEventListener("click", () => {
+  if (input.value == "") {
+    alert("Please provide a phone number");
+  } else if (isValidNumber(input.value)) {
+    const inputValue = input.value;
+    result.innerText = `Valid US number: ${inputValue}`;
+  } else {
+    result.innerText = `Invalid US number: ${inputValue}`;
+  }
+});
+
+clearBtn.addEventListener("click", () => {
+  result.innerText = "";
+});
